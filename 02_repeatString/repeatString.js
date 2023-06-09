4be1ee2e7f7df712b15ae95094e1a2808c40bf42
jrippinger <jrippinger@outlook.com>
2023-06-08 19:03:40 -0700

    Completed Tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    word = "";
 
+    if(num<0){
+        return "ERROR";
+    }
+    else{
+        for(i=0;i<num;i++){
+            word = word.concat(string) 
+        }
+        return word;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
