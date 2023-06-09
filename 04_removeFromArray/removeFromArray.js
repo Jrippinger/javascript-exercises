a9cf6fc2f97dc51124fea66c8bb1883c17885c5a
jrippinger <jrippinger@outlook.com>
2023-06-08 20:33:33 -0700

    Completed Tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for(i=0;i<array.length;i++){
+        for(let n of args){
+            if(array[i] === n || array[i] == n){
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
