f6b7216faf9663eb6fd0e5cb93cdbdf9edbb5372
jrippinger <jrippinger@outlook.com>
2023-06-08 19:35:57 -0700

    Completed Tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    word = "";
 
+    for(i=0;i<string.length;i++){
+        word = word.concat(string.slice(string.length -1 -i,string.length - i))
+    }
+    return word;
 };
 
 // Do not edit below this line
