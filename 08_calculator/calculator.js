2ae079d60466b19c061f3dcb57e031db408f8479
jrippinger <jrippinger@outlook.com>
2023-06-19 18:06:52 -0700

    Completed Tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return(a+b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return(a-b)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let x = 0
+    for(i=0;i<arr.length;i++){
+      x += arr[i]
+    }
+    return(x)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let x = 1
+  if(arr.length == 0){
+    x = 0
+    return(x)
+  }
+  else{
+    for(i=0;i<arr.length;i++){
+      x *= arr[i]
+    }
+    return(x)
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return(a ** b)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  total = 1
+  if(x == 0){
+    return(1)
+  }
+  else{
+    for(i=x;i>0;i--){
+      total *= i
+    }
+    return(total)
+  }
 };
 
 // Do not edit below this line
