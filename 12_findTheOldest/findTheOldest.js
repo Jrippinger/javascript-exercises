7aa731ccf4f342830ad278775a76253364628a32
jrippinger <jrippinger@outlook.com>
2023-06-20 19:51:32 -0700

    Completed tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    
+    age = 0
+    date = new Date()
+    
+    for(i=0;i<arr.length;i++){
+        
+        if(arr[i].hasOwnProperty('yearOfDeath') == false){
+            temp = date.getFullYear() - arr[i].yearOfBirth
 
-};
+            if(temp > age){
+                age = temp
+                old = arr[i]
+            }
+        }
+        else{
+            temp = arr[i].yearOfDeath - arr[i].yearOfBirth
+
+            if(temp > age){
+                age = temp
+                old = arr[i]
+            }
+        }
+    }
+    return(old)
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
