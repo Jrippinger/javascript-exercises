b203cebf840535c5f99a0c16f0aa139998e59711
jrippinger <jrippinger@outlook.com>
2023-06-20 17:39:57 -0700

    Completed Tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    
+    if(typeof x =="string"){
+        x = parseInt(x)
+    }
+    if(x < 0){
+        return("OOPS")
+    }
+    
+    n = 0
+    n1 = 1
+    n2 = 1
 
+    if((x == 1) || (x == 2)){
+        return(1)
+    }
+    else{
+        for(i=0;i<x-2;i++){
+            n = n1 + n2
+            n2 = n1
+            n1 = n
+        }
+        return(n)
+    }
 };
 
 // Do not edit below this line
