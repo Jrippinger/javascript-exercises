01968fa6192009025d5d8759e0f386ff0ad0d8c1
jrippinger <jrippinger@outlook.com>
2023-06-20 17:02:24 -0700

    Completed Tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    str = str.replace(/[&\/\\#,+()$~%.'":*?!<>{} ]/g, '')
+    let x = ""
 
+    for(i=0;i<str.length;i++){
+        x = x + str.substring(str.length - i -1, str.length -i) 
+    }
+
+    if(str == x){
+        return(true)
+    }
+    else{
+        return(false)
+    }
 };
 
 // Do not edit below this line
